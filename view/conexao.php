9a76240993182bfa19186b30752dfcd96522ef84
Dell <Dell@DESKTOP-UF07HOB>
2021-02-19 14:06:01 -0300

    conexão com o banco de dados

--- view/conexao.php
+++ view/conexao.php
@@ -1,5 +1,4 @@
 <?php
-
 $servidor = "127.0.0.1";
 $usuario = "root";
 $senha = "";
@@ -7,5 +6,5 @@ $dbname = "projeto";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-?>
 
+?>
